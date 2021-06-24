adafcc1109a6e774cc8d55302227db7779220ba9
FabianHen <hensslerfabian@gmail.com>
2021-06-24 10:29:18 +0200

    sort und find überarbeitet

--- src/routes.ts
+++ src/routes.ts
@@ -19,115 +19,95 @@ interface Todo {
 
 const todos: Todo[] = JSON.parse(fs.readFileSync("./todos.json", "utf8"));
 
-const routes = Router(); 
+const routes = Router();
 
 routes.get('/', (req, res) => {
     switch (req.query.sortieren) {
         case "prio":
             {
                 const todoPrio: Todo[] = todos;
-
-                let n: number = todoPrio.length
-                while (n > 1) {
-                    for (let i = 0; i <= n - 2; i++) {
-                        if (todoPrio[i].prio < todoPrio[i + 1].prio && todoPrio[i].fertig == 0) {
-                            let hilf: Todo = todoPrio[i]
-                            todoPrio[i] = todoPrio[i + 1]
-                            todoPrio[i + 1] = hilf
-                        }
+                todoPrio.sort(function (a, b) {
+                    if (b.fertig == 0) {
+                        return a.prio - b.prio;
                     }
-                    n--
-                }
+                    return 0
+                });
                 return res.send(todoPrio)
                 break;
+
             }
         case "name":
             {
                 const todoName: Todo[] = todos;
-
-                let n: number = todoName.length
-                while (n > 1) {
-                    for (let i = 0; i <= n - 2; i++) {
-                        if (todoName[i].name > todoName[i + 1].name && todoName[i].fertig == 0) {
-                            let hilf: Todo = todoName[i]
-                            todoName[i] = todoName[i + 1]
-                            todoName[i + 1] = hilf
-                        }
+                todoName.sort(function (a, b) {
+                    var nameA = a.name.toUpperCase();
+                    var nameB = b.name.toUpperCase();
+                    if (nameA < nameB && b.fertig == 0) {
+                        return -1;
                     }
-                    n--
-                }
+                    if (nameA > nameB && b.fertig == 0) {
+                        return 1;
+                    }
+                    return 0;
+                });
                 return res.send(todoName)
                 break;
             }
         case "gruppe":
             {
                 const todoGruppe: Todo[] = todos;
-
-                let n: number = todoGruppe.length
-                while (n > 1) {
-                    for (let i = 0; i <= n - 2; i++) {
-                        if (todoGruppe[i].gruppe > todoGruppe[i + 1].gruppe && todoGruppe[i].fertig == 0) {
-                            let hilf: Todo = todoGruppe[i]
-                            todoGruppe[i] = todoGruppe[i + 1]
-                            todoGruppe[i + 1] = hilf
-                        }
+                todoGruppe.sort(function (a, b) {
+                    var nameA = a.gruppe.toUpperCase();
+                    var nameB = b.gruppe.toUpperCase();
+                    if (nameA < nameB && b.fertig == 0) {
+                        return -1;
                     }
-                    n--
-                }
+                    if (nameA > nameB && b.fertig == 0) {
+                        return 1;
+                    }
+                    return 0;
+                });
                 return res.send(todoGruppe)
                 break;
             }
         case "id":
             {
                 const todoID: Todo[] = todos;
-
-                let n: number = todoID.length
-                while (n > 1) {
-                    for (let i = 0; i <= n - 2; i++) {
-                        if (todoID[i].id > todoID[i + 1].id && todoID [i].fertig == 0) {
-                            let hilf: Todo = todoID[i]
-                            todoID[i] = todoID[i + 1]
-                            todoID[i + 1] = hilf
-                        }
+                todoID.sort(function (a, b) {
+                    if (b.fertig == 0) {
+                        return a.id - b.id;
                     }
-                    n--
-                }
+                    return 0
+                });
                 return res.send(todoID)
                 break;
             }
         case "ende":
             {
                 const todoEnde: Todo[] = todos;
-
-                let n: number = todoEnde.length
-                while (n > 1) {
-                    for (let i = 0; i <= n - 2; i++) {
-                        if (todoEnde[i].ende > todoEnde[i + 1].ende && todoEnde[i].fertig == 0) {
-                            let hilf: Todo = todoEnde[i]
-                            todoEnde[i] = todoEnde[i + 1]
-                            todoEnde[i + 1] = hilf
-                        }
+                todoEnde.sort(function (a, b) {
+                    if (b.fertig == 0) {
+                        return a.ende - b.ende;
                     }
-                    n--
-                }
+                    return 0
+                });
                 return res.send(todoEnde)
                 break;
             }
-            case "erstellt":
+        case "erstellt":
             {
                 const todoErstellt: Todo[] = todos;
-
-                let n: number = todoErstellt.length
-                while (n > 1) {
-                    for (let i = 0; i <= n - 2; i++) {
-                        if (todoErstellt[i].erstellt > todoErstellt[i + 1].erstellt && todoErstellt[i].fertig == 0) {
-                            let hilf: Todo = todoErstellt[i]
-                            todoErstellt[i] = todoErstellt[i + 1]
-                            todoErstellt[i + 1] = hilf
-                        }
+                todoErstellt.sort(function (a, b) {
+                    var nameA = a.erstellt.toUpperCase();
+                    var nameB = b.erstellt.toUpperCase(); m
+                    if (nameA < nameB && b.fertig == 0) {
+                        return -1;
                     }
-                    n--
-                }
+                    if (nameA > nameB && b.fertig == 0) {
+                        return 1;
+                    }
+                    return 0;
+                });
                 return res.send(todoErstellt)
                 break;
             }
@@ -138,27 +118,14 @@ routes.get('/', (req, res) => {
     }
 });
 
-routes.get('/fertig', (req, res)  => 
-{
-    for (let u = 0; u < todos.length; u++) {
-        if (req.query.id == todos[u].id.toString()) {
-            todos[u].fertig = settings[1]
-            settings[1] = settings[1] + 1
-            fs.writeFileSync("./settings.json", JSON.stringify(settings, null, 4));
-        }
-
-    }
-    let n: number = todos.length
-    while (n > 1) {
-        for (let i = 0; i <= n - 2; i++) {
-            if (todos[i].fertig < todos[i + 1].fertig) {
-                let hilf: Todo = todos[i]
-                todos[i] = todos[i + 1]
-                todos[i + 1] = hilf
-            }
-        }
-        n--
-    }
+routes.get('/fertig', (req, res) => {
+    const index: number = todos.id.findIndex(req.query.id)
+    todos[index].fertig = settings[1]
+    settings[1]++
+    todos.sort(function (a, b) {
+        return a.ende - b.ende;
+    });
+    fs.writeFileSync("./settings.json", JSON.stringify(settings, null, 4));
     fs.writeFileSync("./todos.json", JSON.stringify(todos, null, 4));
     return res.send(todos)
 })
@@ -181,10 +148,10 @@ routes.post('/new', (req: Request<unknown, unknown, unknown, Todo>, res) => {
     }
 
     let zeit: string = Date();
-    todos.push({ id: settings[0], name: req.query.name, erstellt: zeit, ende: ende, gruppe: GruppeX, prio: req.query.prio, fertig: 0})
+    todos.push({ id: settings[0], name: req.query.name, erstellt: zeit, ende: ende, gruppe: GruppeX, prio: req.query.prio, fertig: 0 })
     res.send("Erstellt")
     settings[0] = settings[0] + 1
-    
+
     fs.writeFileSync("./settings.json", JSON.stringify(settings, null, 4));
     fs.writeFileSync("./todos.json", JSON.stringify(todos, null, 4));
 })
